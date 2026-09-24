533abddf606e0e54d87587b4b22216b0bdba3943
loctc <loctcu@gmail.com>
2026-09-24 21:37:19 +0700

    Add wedding invitation source

---
+++ src/components/Main.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import useInViewOnce from "../hook/useInViewOnce.js";
+
+const Main = () => {
+  const { ref, show } = useInViewOnce(0.5);
+
+  return (
+    <section ref={ref} className="text-center overflow-hidden px-4">
+      <p
+        className={`
+          font-pinyon
+          text-xl
+          tracking-[1px]
+          transform-gpu
+          will-change-transform
+          transition-[opacity,transform]
+          duration-[5000ms]
+          ease-[cubic-bezier(0.22,1,0.36,1)]
+          ${show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-16"}
+        `}
+      >
+        "Hôn nhân là chuyện cả đời, <br></br>
+        Yêu người vừa ý, cưới người mình thương..."
+      </p>
+      <div className="relative flex flex-col pb-2">
+        {/* box 2 ông bà sui */}
+        <div className="w-full flex justify-between">
+          {/* NHÀ TRAI */}
+          <div
+            className={`
+              transform-gpu
+              will-change-transform
+              transition-[transform,opacity]
+              duration-[5000ms]
+              ease-[cubic-bezier(0.22,1,0.36,1)]
+              ${show ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-28"}
+            `}
+          >
+            <p className="mb-2 font-cafeta text-lg">NHÀ TRAI</p>
+            <p className="mb-0 uppercase font-cafeta text-lg">
+              Ông Lý Hồng Phong
+            </p>
+            <p className="mb-0 uppercase font-cafeta text-lg">
+              Bà Nguyễn Thị Thu Vân
+            </p>
+            <p className="mb-0 font-mono text-sm tracking-[1px]">
+              Tân Châu, An Giang
+            </p>
+          </div>
+          {/* <div className="w-px self-stretch bg-gray-400"></div> */}
+          {/* NHÀ GÁI */}
+          <div
+            className={`
+              transform-gpu
+              will-change-transform
+              transition-[transform,opacity]
+              duration-[5000ms]
+              ease-[cubic-bezier(0.22,1,0.36,1)]
+              ${show ? "opacity-100 translate-x-0" : "opacity-0 translate-x-28"}
+            `}
+          >
+            <p className="mb-2 font-cafeta text-lg">NHÀ GÁI</p>
+            <p className="mb-0 uppercase font-cafeta text-lg">Ông Nguyễn Văn Định</p>
+            <p className="mb-0 uppercase font-cafeta text-lg">Bà Nguyễn Thị Trinh</p>
+            <p className="mb-0 font-mono text-sm tracking-[1px]">
+              Phú Hữu, An Giang
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex justify-center h-20">
+          <img
+            src="https://res.cloudinary.com/dnmv7dodn/image/upload/v1787389875/heart.png"
+            alt="heart"
+            className="absolute -top-4 w-36 animate-heartbeat"
+          />
+        </div>
+
+        {/* box cô dâu & chú rể */}
+        <div className="w-full flex justify-between">
+          <div>
+            <p className="mb-2 font-cormorant text-xl">TRƯỞNG NAM</p>
+            <p
+              className={`
+                text-[55px]
+                font-amsterdam
+                mb-0
+                transform-gpu
+                will-change-transform
+                transition-[transform,opacity]
+                duration-[5000ms]
+                ease-[cubic-bezier(0.22,1,0.36,1)]
+                ${show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-16"}
+              `}
+            >
+              Lý Hoàng Huy
+            </p>
+          </div>
+          <div>
+            <p className="mb-2 font-cormorant text-xl">ÚT NỮ</p>
+            <p
+              className={`
+                text-[55px]
+                font-amsterdam
+                mb-0
+                transform-gpu
+                will-change-transform
+                transition-[transform,opacity]
+                duration-[5000ms]
+                ease-[cubic-bezier(0.22,1,0.36,1)]
+                ${show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-16"}
+              `}
+            >
+              Nguyễn Thị Anh Thư
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex bg-gray-100 gap-3 p-3">
+        <div className="flex justify-center w-1/2">
+          <img
+            src="https://res.cloudinary.com/dnmv7dodn/image/upload/v1787389559/CHURE.webp"
+            alt="chú rể"
+            className={`
+              w-full
+              h-auto
+              transform-gpu
+              will-change-transform
+              transition-[transform,opacity]
+              duration-[5000ms]
+              ease-[cubic-bezier(0.22,1,0.36,1)]
+              ${show ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-28"}
+            `}
+          />
+        </div>
+
+        <div className="flex justify-center w-1/2">
+          <img
+            src="https://res.cloudinary.com/dnmv7dodn/image/upload/v1787389561/CODAU.webp"
+            alt="codau"
+            className={`
+              w-full
+              h-auto
+              transform-gpu
+              will-change-transform
+              transition-[transform,opacity]
+              duration-[5000ms]
+              ease-[cubic-bezier(0.22,1,0.36,1)]
+              ${show ? "opacity-100 translate-x-0" : "opacity-0 translate-x-28"}
+            `}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
